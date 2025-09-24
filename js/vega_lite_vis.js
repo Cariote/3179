2cb1eac34fabeb89f270cfd87a46caaa5a9d1a44
cang0015 <cang0015@student.monash.edu>
2025-09-25 02:45:08 +0800

    Update vega_lite_vis.js urls

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "./data/daily_cost_bar_chart.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/Cariote/3179/refs/heads/homework-week9/data/daily_cost_bar_chart.vg.json";
 
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
     // Access the Vega view instance
