d24b308bfd289965a5ed75a89355ea6a9cd8ab1f
cang0015 <cang0015@student.monash.edu>
2025-09-25 02:41:06 +0800

    Refactor files into each folder

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "daily_cost_bar_chart.vg.json";
+var vg_1 = "./data/daily_cost_bar_chart.vg.json";
 
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
     // Access the Vega view instance
