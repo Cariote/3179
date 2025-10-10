e89ffce78f6d903917853afc99f93eac8d16ae06
cang0015 <cang0015@student.monash.edu>
2025-10-10 17:56:35 +0800

    Commit homework week 10 files

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,5 +1,10 @@
-var vg_1 = "https://raw.githubusercontent.com/Cariote/3179/refs/heads/homework-week9/data/daily_cost_bar_chart.vg.json";
+var vg_1 = "data/daily_cost_bar_chart.vg.json";
+var vg_2 = "data/worldwide_gdp_per_capita.vg.json";
 
 vegaEmbed("#map_chart", vg_1).then(function(result) {
+    // Access the Vega view instance
+}).catch(console.error);
+
+vegaEmbed("#gdppc_chart", vg_2).then(function(result) {
     // Access the Vega view instance
 }).catch(console.error);
