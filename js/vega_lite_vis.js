00c7f04ba2a68b406ae25a0b805c959486ba02ac
cang0015 <cang0015@student.monash.edu>
2025-10-14 10:12:38 +0800

    Vis 2

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,5 +1,7 @@
-var vg_1 = "data/daily_cost_bar_chart.vg.json";
-var vg_2 = "data/worldwide_gdp_per_capita.vg.json";
+var vg_1 = "charts/daily_cost_bar_chart.vg.json";
+var vg_2 = "charts/worldwide_gdp_per_capita.vg.json";
+var vg_3 = "charts/gdp_per_capita_computing.vg.json";
+var vg_4 = "charts/computer_education.vg.json";
 
 vegaEmbed("#map_chart", vg_1).then(function(result) {
     // Access the Vega view instance
@@ -8,3 +10,9 @@ vegaEmbed("#map_chart", vg_1).then(function(result) {
 vegaEmbed("#gdppc_chart", vg_2).then(function(result) {
     // Access the Vega view instance
 }).catch(console.error);
+
+vegaEmbed("#gdpcomp_chart", vg_3).then(function(result) {
+    // Access the Vega view instance
+}).catch(console.error);
+
+vegaEmbed("#compedu_chart", vg_4).then(function(result){}).catch(console.error);
